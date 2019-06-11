9b2ee2bfb60e4160dbdad581c0951a6b9938b3c4
Aiden McClelland <aiden.mcclelland@saltlending.com>
2019-06-11 12:28:57 -0600

    HashRouter

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter } from "react-router-dom"
+import { HashRouter } from "react-router-dom"
 import App from './app/App'
 import * as serviceWorker from './serviceWorker'
 
 ReactDOM.render(
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>,
+  </HashRouter>,
   document.getElementById('root'),
 )
 
